2e4c52fde3025032cb2d8f464a372b6fb3b3beda
brianq-nisra <79097487+brianq-nisra@users.noreply.github.com>
2023-08-03 16:43:45 +0100

    domains screen

---
+++ domains_data.js
@@ -0,0 +1,41 @@
+var domains_data = {
+    "Happier Children": {
+       "description": "Our children and young people have the best start in life",
+       "indicators": ["Childrens social care",
+          "Pre-school readiness",
+          "Small for gestational age"
+       ]
+    },
+    "Cleaner Environment": {
+       "description": "Climate change",
+       "indicators": []
+    },
+    "Equal Society": {
+       "description": "Everyone being included fairly and equally",
+       "indicators": []
+    },
+    "Healthier Lives": {
+       "description": "Good health",
+       "indicators": []
+    },
+    "Brighter Futures": {
+       "description": "Everyone having the chance to do well in life",
+       "indicators": []
+    },
+    "Better Economy": {
+       "description": "This is how well people in Northern Ireland are working, creating jobs and making money",
+       "indicators": []
+    },
+    "Safer Communities": {
+       "description": "Feeling safe and secure",
+       "indicators": []
+    },
+    "Caring Society": {
+       "description": "Having respect for each other",
+       "indicators": []
+    },
+    "Better Homes": {
+       "description": "Living in a nice place",
+       "indicators": []
+    }
+ }
