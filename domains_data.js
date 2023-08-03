77ad2792c4e39fa94f6e82e4f4e850859e5fd5dd
brianq-nisra <79097487+brianq-nisra@users.noreply.github.com>
2023-08-03 16:57:45 +0100

    Updates to data

--- domains_data.js
+++ domains_data.js
@@ -2,40 +2,67 @@ var domains_data = {
     "Happier Children": {
        "description": "Our children and young people have the best start in life",
        "indicators": ["Childrens social care",
-          "Pre-school readiness",
-          "Small for gestational age"
+            "Pre-school readiness",
+            "Small for gestational age"
        ]
     },
     "Cleaner Environment": {
        "description": "Climate change",
-       "indicators": []
+       "indicators": ["Air pollution",
+            "Greenhouse gas emissions",
+            "Low carbon and renewable energy employment",
+            "Recycled waste",
+            "River water quality"]
     },
     "Equal Society": {
        "description": "Everyone being included fairly and equally",
-       "indicators": []
+       "indicators": ["Cultural identity",
+            "Income deprivation",
+            "Income inequality",
+            "Respect"
+        ]
     },
     "Healthier Lives": {
        "description": "Good health",
-       "indicators": []
+       "indicators": ["Healthy life expectancy (females)",
+            "Healthy life expectancy (males)",
+            "Mental health",
+            "Preventable deaths",
+            "Sports and physical activity"]
     },
     "Brighter Futures": {
        "description": "Everyone having the chance to do well in life",
-       "indicators": []
+       "indicators": ["Arts and cultural activities",
+            "Life satisfaction",
+            "School leavers' attainment gap",
+            "Self-efficacy"]
     },
     "Better Economy": {
        "description": "This is how well people in Northern Ireland are working, creating jobs and making money",
-       "indicators": []
+       "indicators": ["Economic inactivity",
+            "Employment rate",
+            "External sales rate",
+            "Local graduates in narrow STEM subjects",
+            "International reputation",
+            "Northern Ireland Composite Economic Index",
+            "Skills"]
     },
     "Safer Communities": {
        "description": "Feeling safe and secure",
-       "indicators": []
+       "indicators": ["Crime prevalence rate",
+            "Reoffending rate"]
     },
     "Caring Society": {
        "description": "Having respect for each other",
-       "indicators": []
+       "indicators": ["Adult social care",
+            "Loneliness",
+            "Volunteering"]
     },
     "Better Homes": {
        "description": "Living in a nice place",
-       "indicators": []
+       "indicators": ["Homelessness",
+            "Housing costs as a proportion of household income",
+            "Housing stress"
+        ]
     }
  }
