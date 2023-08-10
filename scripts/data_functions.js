6a463592ddbbe2e04f68c2ca8c0217349877f1d7
brianq-nisra <79097487+brianq-nisra@users.noreply.github.com>
2023-08-10 15:24:43 +0100

    Some data edits

--- scripts/data_functions.js
+++ scripts/data_functions.js
@@ -4,8 +4,8 @@ function createLineChart (id, title, statistic, geography, matrix) {
         "autoupdate": true,
         "matrix": null,
         "type": "line",
-        "copyright": true,
-        "link": "https://ppdata.nisra.gov.uk/table/" + matrix,
+        "copyright": false,
+        "link": `https://ppdata.nisra.gov.uk/table/${matrix}`,
         "sort": false,
         "metadata": {
            "xAxis": {
@@ -149,7 +149,7 @@ function createLineChart (id, title, statistic, geography, matrix) {
                  "tension": 0.4
               }
            },
-           "updated": "2023-07-27T10:03:00.857Z",
+           "updated": "",
            "layout": {
               "padding": {
                  "left": 22,
@@ -175,128 +175,168 @@ for (let i = 0; i < domains.length; i++) {
 
         var NI_matrix = Object.values(indicators)[j].data.NI;
         var LGD_matrix = Object.values(indicators)[j].data.LGD;
+        var EQ_matrix = Object.values(indicators)[j].data.EQ;
 
         if (NI_matrix != "") {
+
             this_matrix = NI_matrix;
             this_geography = "NI";
-            this_statistic = this_matrix.replace("NI", "");
-        } else {
+
+            if (["INDGREENHGNI", "INDNICEINI", "INDRIVERQNI"].includes(this_matrix)) {
+               this_statistic = this_matrix;
+            } else {
+               this_statistic = this_matrix.substring(0, this_matrix.length - 2);
+            }
+
+            
+
+        } else if (LGD_matrix != "") {
+
             this_matrix = LGD_matrix;
-            this_geography = "LGD2014"
-            this_statistic = this_matrix.replace("LGD", "");
+            this_geography = "LGD2014";
+            this_statistic = this_matrix.substring(0, this_matrix.length - 3);
+
+        } else if (EQ_matrix != "") {
+
+            this_matrix = LGD_matrix;
+            this_geography = "EQUALGROUPS";
+            this_statistic = this_matrix.substring(0, this_matrix.length - 2);  
+
+        } else {
+         this_matrix = ""
         }
 
-        var this_id = this_statistic + "-line";
+        if (this_matrix != "") {
 
-        chart_container = document.createElement("div");
-        chart_container.id = this_id;
-        chart_container.classList.add("pxwidget");
-        chart_container.classList.add("line-chart");
-        chart_container.style.display = "none";
+         var this_id = this_statistic + "-line";
 
-        document.getElementById("line-chart").appendChild(chart_container);
+         chart_container = document.createElement("div");
+         chart_container.id = this_id;
+         chart_container.classList.add("pxwidget");
+         chart_container.classList.add("line-chart");
+         chart_container.style.display = "none";
 
-        createLineChart(id = this_statistic + "-line",
+         document.getElementById("line-chart").appendChild(chart_container);
+
+         createLineChart(id = this_id,
             title = Object.keys(indicators)[j],
             statistic = this_statistic,
             geography = this_geography,
-            matrix = this_matrix)    
+            matrix = this_matrix)
+
+        }          
 
     }
 
 }
 
-function createMap(id, title, matrix, indicator) {
+function rewriteLinks() {
 
-    pxWidget.queue('map', id, {
-        "autoupdate": true,
-        "matrix": null,
-        "mapDimension": "LGD2014",
-        "copyright": true,
-        "link": `<a href="https://ppdata.nisra.gov.uk/table/${matrix}" target="_blank">See data here</a>`,
-        "title": title,
-        "borders": true,
-        "colorScale": "red",
-        "tooltipTitle": null,
-        "fullScreen": {
-        "title": "View Fullscreen",
-        "titleCancel": "Exit Fullscreen",
-        "center": [54.65, -6.8]
-        },
-        "data": {
-        "datasets": [{
-            "api": {
-                "query": {
-                    "url": "https://ppws-data.nisra.gov.uk/public/api.jsonrpc",
-                    "data": {
-                    "jsonrpc": "2.0",
-                    "method": "PxStat.Data.Cube_API.ReadDataset",
-                    "params": {
-                        "class": "query",
-                        "id": ["STATISTIC", "TLIST(A1)"],
-                        "dimension": {
-                            "STATISTIC": {
-                                "category": {
-                                "index": [indicator]
-                                }
-                            },
-                            "TLIST(A1)": {
-                                "category": {
-                                "index": ["2020/21"]
-                                }
-                            }
-                        },
-                        "extension": {
-                            "language": {
-                                "code": "en"
-                            },
-                            "format": {
-                                "type": "JSON-stat",
-                                "version": "2.0"
-                            },
-                            "matrix": matrix
-                        },
-                        "version": "2.0",
-                        "m2m": false
-                    }
-                    }
-                },
-                "response": {}
-            },
-            "fluidTime": [0]
-        }]
-        },
-        "metadata": {
-        "api": {
-            "query": {
-                "url": "https://ppws-data.nisra.gov.uk/public/api.jsonrpc",
-                "data": {
-                    "jsonrpc": "2.0",
-                    "method": "PxStat.Data.Cube_API.ReadMetadata",
-                    "params": {
-                    "matrix": matrix,
-                    "language": "en",
-                    "format": {
-                        "type": "JSON-stat",
-                        "version": "2.0"
-                    }
-                    },
-                    "version": "2.0"
-                }
-            },
-            "response": {}
-        }
-        },
-        "options": {
-        "mode": "e",
-        },
-        "baseMap": {
-        "leaflet": [],
-        "esri": []
-        }
-    });
+   var links = document.getElementsByTagName("a");
+
+   for (let i = 0; i < links.length; i++) {
+
+       if (links[i].innerHTML.includes("ppdata")) {
+         links[i].innerHTML = "See data on NISRA Data Portal";
+       }
+
+   }
 
 }
+
+document.onclick = rewriteLinks;
+
+// function createMap(id, title, matrix, indicator) {
+
+//     pxWidget.queue('map', id, {
+//         "autoupdate": true,
+//         "matrix": null,
+//         "mapDimension": "LGD2014",
+//         "copyright": true,
+//         "link": `<a href="https://ppdata.nisra.gov.uk/table/${matrix}" target="_blank">See data here</a>`,
+//         "title": title,
+//         "borders": true,
+//         "colorScale": "red",
+//         "tooltipTitle": null,
+//         "fullScreen": {
+//         "title": "View Fullscreen",
+//         "titleCancel": "Exit Fullscreen",
+//         "center": [54.65, -6.8]
+//         },
+//         "data": {
+//         "datasets": [{
+//             "api": {
+//                 "query": {
+//                     "url": "https://ppws-data.nisra.gov.uk/public/api.jsonrpc",
+//                     "data": {
+//                     "jsonrpc": "2.0",
+//                     "method": "PxStat.Data.Cube_API.ReadDataset",
+//                     "params": {
+//                         "class": "query",
+//                         "id": ["STATISTIC", "TLIST(A1)"],
+//                         "dimension": {
+//                             "STATISTIC": {
+//                                 "category": {
+//                                 "index": [indicator]
+//                                 }
+//                             },
+//                             "TLIST(A1)": {
+//                                 "category": {
+//                                 "index": ["2020/21"]
+//                                 }
+//                             }
+//                         },
+//                         "extension": {
+//                             "language": {
+//                                 "code": "en"
+//                             },
+//                             "format": {
+//                                 "type": "JSON-stat",
+//                                 "version": "2.0"
+//                             },
+//                             "matrix": matrix
+//                         },
+//                         "version": "2.0",
+//                         "m2m": false
+//                     }
+//                     }
+//                 },
+//                 "response": {}
+//             },
+//             "fluidTime": [0]
+//         }]
+//         },
+//         "metadata": {
+//         "api": {
+//             "query": {
+//                 "url": "https://ppws-data.nisra.gov.uk/public/api.jsonrpc",
+//                 "data": {
+//                     "jsonrpc": "2.0",
+//                     "method": "PxStat.Data.Cube_API.ReadMetadata",
+//                     "params": {
+//                     "matrix": matrix,
+//                     "language": "en",
+//                     "format": {
+//                         "type": "JSON-stat",
+//                         "version": "2.0"
+//                     }
+//                     },
+//                     "version": "2.0"
+//                 }
+//             },
+//             "response": {}
+//         }
+//         },
+//         "options": {
+//         "mode": "e",
+//         },
+//         "baseMap": {
+//         "leaflet": [],
+//         "esri": []
+//         }
+//     });
+
+// }
 
 
 
