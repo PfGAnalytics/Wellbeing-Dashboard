088b950804d8b0d311361f86339d2123d30aa69d
brianq-nisra <79097487+brianq-nisra@users.noreply.github.com>
2023-08-16 15:25:46 +0100

    Update source url's to be non-year specific

--- scripts/data_functions.js
+++ scripts/data_functions.js
@@ -108,7 +108,9 @@ async function createLineChart (id, title, statistic, geography, matrix, y_label
                  "ticks": {
                     "beginAtZero": false,
                     "maxTicksLimit": null,
-                    "reverse": false
+                    "reverse": false,
+                    "minRotation": 0,
+                    "maxRotation": 0
                  },
                  "gridLines": {
                     "display": false
@@ -363,107 +365,3 @@ for (let i = 0; i < domains.length; i++) {
     }
 
 }
-
-// function createMap(id, title, matrix, indicator) {
-
-// pxWidget.queue('map', id, {
-//    "autoupdate": true,
-//    "matrix": null,
-//    "mapDimension": "LGD2014",
-//    "copyright": true,
-//    "link": `<a href="https://ppdata.nisra.gov.uk/table/${matrix}" target="_blank">See data here</a>`,
-//    "title": title,
-//    "borders": true,
-//    "colorScale": "red",
-//    "tooltipTitle": null,
-//    "fullScreen": {
-//    "title": "View Fullscreen",
-//    "titleCancel": "Exit Fullscreen",
-//    "center": [54.65, -6.8]
-//    },
-//    "data": {
-//    "datasets": [{
-//        "api": {
-//            "query": {
-//                "url": "https://ppws-data.nisra.gov.uk/public/api.jsonrpc",
-//                "data": {
-//                "jsonrpc": "2.0",
-//                "method": "PxStat.Data.Cube_API.ReadDataset",
-//                "params": {
-//                    "class": "query",
-//                    "id": ["STATISTIC", "TLIST(A1)"],
-//                    "dimension": {
-//                        "STATISTIC": {
-//                            "category": {
-//                            "index": [indicator]
-//                            }
-//                        },
-//                        "TLIST(A1)": {
-//                            "category": {
-//                            "index": ["2020/21"]
-//                            }
-//                        }
-//                    },
-//                    "extension": {
-//                        "language": {
-//                            "code": "en"
-//                        },
-//                        "format": {
-//                            "type": "JSON-stat",
-//                            "version": "2.0"
-//                        },
-//                        "matrix": matrix
-//                    },
-//                    "version": "2.0",
-//                    "m2m": false
-//                }
-//                }
-//            },
-//            "response": {}
-//        },
-//        "fluidTime": [0]
-//    }]
-//    },
-//    "metadata": {
-//    "api": {
-//        "query": {
-//            "url": "https://ppws-data.nisra.gov.uk/public/api.jsonrpc",
-//            "data": {
-//                "jsonrpc": "2.0",
-//                "method": "PxStat.Data.Cube_API.ReadMetadata",
-//                "params": {
-//                "matrix": matrix,
-//                "language": "en",
-//                "format": {
-//                    "type": "JSON-stat",
-//                    "version": "2.0"
-//                }
-//                },
-//                "version": "2.0"
-//            }
-//        },
-//        "response": {}
-//    }
-//    },
-//    "options": {
-//    "mode": "e",
-//    },
-//    "baseMap": {
-//    "leaflet": [],
-//    "esri": []
-//    }
-// });
-
-// }
-
-
-
-//    createMap(id = "INDSLATTGAPLGD-map",
-//             title = "Indicator - School leavers' attainment gap",
-//             matrix = "INDSLATTGAPLGD",
-//             indicator = "INDSLATTGAP")
-
-
-
-
-
