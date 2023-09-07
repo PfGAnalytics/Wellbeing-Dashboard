ecdf79142a843fd8b5d6cb5340180fc8ce40481f
brianq-nisra <brian.quinn@nisra.gov.uk>
2023-09-07 11:08:58 +0100

    https source for map tile

--- scripts/data_functions.js
+++ scripts/data_functions.js
@@ -767,7 +767,7 @@ async function drawMap(matrix, improvement, title) {
 
        L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
            maxZoom: 19,
-           attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+           attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a>'
        }).addTo(map); // Add a background map
 
        // Colour palettes for increasing/decreasing indicators
