fc32fba94e253692062d3eca371b9ba84562e128
SarahCrossey <sarah.crossey@executiveoffice-ni.gov.uk>
2025-08-11 14:09:00 +0100

    Homelessness updated

--- scripts/domains_data.js
+++ scripts/domains_data.js
@@ -961,7 +961,7 @@ var domains_data = {
             telling: {
                improved: "This commentary is a placeholder.",
                no_change: "This commentary is a placeholder.",
-               worsened: "Since 2021/22 the number of Households that have been accepted as homeless has risen. All of those who are accepted as statutorily homeless have a statutory duty to temporary accommodation and there has also been an increase from 9,265 placements in 2021/22 to 11,368 in 2023/24.  The number of households accepted as homeless is an annual total for each given year whereas the number of households placed in Temporary Accommodation is a cumulative figure that will include households that were accepted as homeless in previous years.  Not all households accepted as homeless will be allocated a home within the same year.",
+               worsened: "From 2021/22 until 2023/24, the number of households that have been accepted as homeless rose year on year. While the number of households accepted as homeless has decreased in 2024/25, it still remains higher than the position in 2021/22. All of those who are accepted as statutorily homeless have a statutory duty to temporary accommodation and there has been an increase in the number of placements in temporary accommodation over the period, from 9,265 placements in 2021/22 to 11,887 in 2024/25.  The number of households accepted as homeless is an annual total for each given year whereas the number of households placed in Temporary Accommodation is a cumulative figure that will include households that were accepted as homeless in previous years.  Not all households accepted as homeless will be allocated a home within the same year.",
                insufficient: ""
             }
          },
