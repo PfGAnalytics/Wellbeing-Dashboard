3e7d39ae208eff7f48942d4970f6f6ad25a484b0
jacksonmckee-nisra <Jackson.McKee@nisra.gov.uk>
2025-10-09 12:43:48 +0100

    Colour palette for bar charts updated

--- scripts/data_functions.js
+++ scripts/data_functions.js
@@ -1534,7 +1534,7 @@ async function getEqualityGroups(d, e) {
          };         
 
          // Colour palette for bar charts:
-         colours = ["#12436D", "#28A197", "#801650", "#F46A25", "#3D3D3D", "#A285D1", "#0A4D46","#F66068","#472C4C"];
+         colours = ["#3878c5", "#00205b", "#68a41e", "#732777", "#ce70d2", "#434700", "#a88f8f","#3b3b3b","#e64791", "#400b23"];
 
          for (let j = 0; j < Object.keys(values).length; j ++) {     // Loop through values and create each data series
             data.datasets.push({
