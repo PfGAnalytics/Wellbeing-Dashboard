b1736948debbe832b9f8c310f77bc63c1a25a8cc
JVL-TEO <joshua.vanloggerenberg@executiveoffice-ni.gov.uk>
2026-03-09 09:49:12 +0000

    Sport - Typo fix

--- scripts/domains_data.js
+++ scripts/domains_data.js
@@ -480,7 +480,7 @@ var domains_data = {
             telling: {
                improved: "This commentary is a placeholder.",
                no_change: "This commentary is a placeholder.",
-               worsened: "The comparison year has been set to 2021/22 due to changes in the data collection method as a result of Covid-19. While comparisons can be made between 2021/22 and 2024/25, it is important to note that participation in sport and physical activity in 2021/22 would have been impacted by closures due to COVID-19. Between 2021/22 and 2024/25 there have been consistent gaps in participation in sports and physical activity between adults with a disability compared with those without, adults with dependants compared with those without and for people living in the least and most deprived areas.",
+               worsened: "The comparison year has been set to 2021/22 due to changes in the data collection method as a result of Covid-19. While comparisons can be made between 2021/22 and 2024/25, it is important to note that participation in sport and physical activity in 2021/22 would have been impacted by closures due to Covid-19. Between 2021/22 and 2024/25 there have been consistent gaps in participation in sports and physical activity between adults with a disability compared with those without, adults with dependants compared with those without and for people living in the least and most deprived areas.",
                insufficient: ""
             }
          }
