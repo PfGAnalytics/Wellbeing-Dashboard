16f70aa6da20c223e0cfe254563ed3154ff90b85
jacksonmckee-nisra <Jackson.McKee@nisra.gov.uk>
2026-03-09 09:22:15 +0000

    navbar draft

--- scripts/navigation_functions.js
+++ scripts/navigation_functions.js
@@ -1214,8 +1214,6 @@ function sizeForMobile() {
         }
 
         map_container.style.marginLeft = ((window.innerWidth - 700) / 2) + "px";        // Re-position map in middle of screen
-        top_menu_items_div.style.marginBottom = "20px";                                 // Extra space underneath top menu items
-        top_menu_items_div.style.marginTop= "10px";                                     // Extra space above top menu items
         top_menu_items_div.style.width = "100%";                                        // Move top menu onto its own row by setting it to full width
         // dashboard_title.style.width = (top_container.clientWidth - 300) + "px";         // Space for title to 300px less than window width
         for (let i = 0; i < grey_box.length; i++) {
