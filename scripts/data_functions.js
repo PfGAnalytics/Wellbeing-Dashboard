7bc10d1f6bd8a4c23b763b11a5d3106309309d63
danielnisra <daniel.nelson-donaghy@nisra.gov.uk>
2026-05-06 09:01:57 +0100

    hding navy oval on good jobs industry sector download

--- scripts/data_functions.js
+++ scripts/data_functions.js
@@ -1908,7 +1908,7 @@ async function renderPopup (d, e, eq_group) {
 
             // Hide 'click legend item with a white shape
             const maskTop = titleTop + titleH;
-            const maskHeight = 50;
+            const maskHeight = 55;
             ctx.fillStyle = "#fff";
             ctx.fillRect(leftInset, maskTop, renderedWidth, maskHeight);
 
