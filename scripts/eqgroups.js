dd8ee15c9c7d3a79409242bc7448b87ed5679a73
brianq-nisra <brian.quinn@nisra.gov.uk>
2026-08-04 12:23:27 +0100

    add comments to eqgroups.js

--- scripts/eqgroups.js
+++ scripts/eqgroups.js
@@ -1,26 +1,179 @@
 var eqgroups = {
-  "Sex": ["1", "2"],
-  "Age": ["76", "59", "45", "46", "47", "48", "49", "99", "3", "38", "50", "52", "37", "60", "53", "100", "54", "4", "61", "55", "69", "5", "56", "70", "57", "6", "71", "62", "58", "7", "72", "36", "8", "51"],
-  "Marital status": ["9", "10", "41", "11", "12", "13", "39", "128", "129"],
-  "Dependants": ["17", "18"],
-  "Disability": ["19", "20"],
-  "Ethnic group": ["21", "22"],
-  "Sexual orientation": ["23", "24"],
-  "Deprivation": ["25", "26", "27", "28", "29", "40"],
-  "Urban Rural": ["30", "31", "32"],
-  "Political opinion": ["33", "34", "35"],
-  "Religion": ["14", "15", "16"],
-  "Household Group": ["63", "64", "65", "66", "67", "68", "93", "94", "95", "96", "97", "98"],
-  "Tenure": ["42", "43", "44", "75"],
-  "Special Educational Needs": ["73", "74"],
-  "Offence category": ["81", "82", "83", "84", "85", "86", "87"],
-  "Court type": ["88", "89", "90"],
-  "Year group": ["77", "78", "79", "80"],
-  "Free School Meals": ["91", "92"],
-  "Industry Sector": ["101", "102", "103", "104", "105", "106", "107", "108", "109"],
-  "Occupation": ["110", "111", "112", "113", "114", "115", "116", "117", "118"],
-  "Work pattern": ["123", "124"],
-  "Work pattern by Sex": ["119", "120", "121", "122"],
-  "Volunteering type": ["125", "126"],
-  "Children in poverty": ["127"]
+  "Sex": [
+    "1",   // Male
+    "2"   // Female
+  ],
+  "Age": [
+    "76",   // 0-15
+    "59",   // Under 18
+    "45",   // Youth
+    "46",   // 10-13
+    "47",   // 14
+    "48",   // 15
+    "49",   // 16
+    "99",   // 16-21
+    "3",   // 16-24
+    "38",   // 16-34
+    "50",   // 17
+    "52",   // 18-19
+    "37",   // 18-24
+    "60",   // 18-25
+    "53",   // 20-24
+    "100",   // 22-29
+    "54",   // 25-29
+    "4",   // 25-34
+    "61",   // 26-29
+    "55",   // 30-39
+    "69",   // 35-44
+    "5",   // 35-49
+    "56",   // 40-49
+    "70",   // 45-54
+    "57",   // 50-59
+    "6",   // 50-64
+    "71",   // 55-64
+    "62",   // 60-64
+    "58",   // 60+
+    "7",   // 65-74
+    "72",   // 65-74
+    "36",   // 65+
+    "8",   // 75+
+    "51"   // Adult
+  ],
+  "Marital status": [
+    "9",   // Single
+    "10",   // Married/Civil partnership
+    "41",   // Married/Civil partnership/Cohabiting
+    "11",   // Separated
+    "12",   // Divorced
+    "13",   // Widowed
+    "39",   // Divorced/Separated
+    "128",   // Cohabiting
+    "129"   // Other
+  ],
+  "Dependants": [
+    "17",   // With dependants (overall)
+    "18"   // Without dependants
+  ],
+  "Disability": [
+    "19",   // Yes
+    "20"   // No
+  ],
+  "Ethnic group": [
+    "21",   // White
+    "22"   // Other
+  ],
+  "Sexual orientation": [
+    "23",   // Heterosexual
+    "24"   // Gay/lesbian, bisexual and other
+  ],
+  "Deprivation": [
+    "25",   // Quintile 1 - Most deprived
+    "26",   // Quintile 2
+    "27",   // Quintile 3
+    "28",   // Quintile 4
+    "29",   // Quintile 5 - Least deprived
+    "40"   // Quintile 4/5
+  ],
+  "Urban Rural": [
+    "30",   // Urban
+    "31",   // Rural
+    "32"   // Mixed
+  ],
+  "Political opinion": [
+    "33",   // Unionist
+    "34",   // Nationalist
+    "35"   // Neither
+  ],
+  "Religion": [
+    "14",   // Catholic
+    "15",   // Protestant
+    "16"   // Other/no religion
+  ],
+  "Household Group": [
+    "63",   // Elderly
+    "64",   // Large Adult
+    "65",   // Large Family
+    "66",   // Single
+    "67",   // Small Adult
+    "68",   // Small Family
+    "93",   // Single Male
+    "94",   // Single Female
+    "95",   // Couples
+    "96",   // Families
+    "97",   // Pensioner households
+    "98"   // Undefined
+  ],
+  "Tenure": [
+    "42",   // Private Rented
+    "43",   // Social Rented
+    "44",   // Buying with Mortgage
+    "75"   // Owner Occupied
+  ],
+  "Special Educational Needs": [
+    "73",   // Yes
+    "74"   // No
+  ],
+  "Offence category": [
+    "81",   // VAP
+    "82",   // Sexual
+    "83",   // Robbery/ Theft/ Burglary
+    "84",   // Criminal Damage/ Public Order
+    "85",   // Drugs
+    "86",   // Motoring
+    "87"   // Other
+  ],
+  "Court type": [
+    "88",   // Crown Court
+    "89",   // Adult magistrates' courts
+    "90"   // Youth magistrates' courts
+  ],
+  "Year group": [
+    "77",   // Year 9
+    "78",   // Year 10
+    "79",   // Year 11
+    "80"   // Year 12
+  ],
+  "Free School Meals": [
+    "91",   // Entitled
+    "92"   // Not Entitled
+  ],
+  "Industry Sector": [
+    "101",   // Agriculture, Forestry and Fishing
+    "102",   // Energy and Water
+    "103",   // Manufacturing
+    "104",   // Construction
+    "105",   // Distribution, Hotels and Restaurants
+    "106",   // Transport and Communication
+    "107",   // Banking and Finance
+    "108",   // Public Admin, Education and Health
+    "109"   // Other Services
+  ],
+  "Occupation": [
+    "110",   // Managers, directors and senior officials
+    "111",   // Professional occupations
+    "112",   // Associate professional occupations
+    "113",   // Administrative and secretarial occupations
+    "114",   // Skilled trades occupations
+    "115",   // Caring, leisure and other service occupations
+    "116",   // Sales and customer service occupations
+    "117",   // Process, plant and machine operatives
+    "118"   // Elementary occupations
+  ],
+  "Work pattern": [
+    "123",   // Total full-time
+    "124"   // Total part-time
+  ],
+  "Work pattern by Sex": [
+    "119",   // Male full-time
+    "120",   // Male part-time
+    "121",   // Female full-time
+    "122"   // Female part-time
+  ],
+  "Volunteering type": [
+    "125",   // Formal
+    "126"   // Informal
+  ],
+  "Children in poverty": [
+    "127"   // Children
+  ]
 };
