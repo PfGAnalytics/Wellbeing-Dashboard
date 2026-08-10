362995c7ba9a5bc89db9368cf70e232e1898b435
brianq-nisra <brian.quinn@nisra.gov.uk>
2026-08-10 12:19:50 +0100

    add new eq groups

--- scripts/eqgroups.js
+++ scripts/eqgroups.js
@@ -1,9 +1,44 @@
 var eqgroups = {
   "Sex": [
+   "EQ201",   // Female
+   "EQ202",   // Male
     "1",   // Male
     "2"   // Female
   ],
-  "Age": [
+  "Age": [	
+   "EQ101",   // 0-15
+   "EQ127",   // 0-17
+   "EQ116",   // 19 and under
+   "EQ119",   // 2-10
+   "EQ126",   // 3-15
+   "EQ120",   // 11-15
+   "EQ129",   // 16-21
+   "EQ102",   // 16-24
+   "EQ107",   // 16-34
+   "EQ133",   // 16-39
+   "EQ121",   // 16+
+   "EQ115",   // 18-24
+   "EQ122",   // 18-29
+   "EQ128",   // 18+
+   "EQ117",   // 20-24
+   "EQ130",   // 22-29
+   "EQ111",   // 25-34
+   "EQ103",   // 25-44
+   "EQ118",   // 25+
+   "EQ123",   // 30-39
+   "EQ112",   // 35-44
+   "EQ108",   // 35-49
+   "EQ124",   // 40-49
+   "EQ113",   // 45-54
+   "EQ104",   // 45-64
+   "EQ131",   // 50-59
+   "EQ109",   // 50-64
+   "EQ125",   // 50+
+   "EQ114",   // 55-64
+   "EQ132",   // 60-64
+   "EQ105",   // 65-74
+   "EQ110",   // 65+
+   "EQ106",   // 75+
     "76",   // 0-15
     "59",   // Under 18
     "45",   // Youth
@@ -40,6 +75,16 @@ var eqgroups = {
     "51"   // Adult
   ],
   "Marital status": [
+   "EQ301",   // Single
+   "EQ310",   // Married
+   "EQ302",   // Married/Civil partnership
+   "EQ308",   // Married/Civil partnership/Cohabiting
+   "EQ303",   // Separated
+   "EQ304",   // Divorced
+   "EQ305",   // Widowed
+   "EQ306",   // Divorced/Separated
+   "EQ307",   // Other
+   "EQ309",   // Unknown
     "9",   // Single
     "10",   // Married/Civil partnership
     "41",   // Married/Civil partnership/Cohabiting
@@ -51,18 +96,44 @@ var eqgroups = {
     "129"   // Other
   ],
   "Dependants": [
+   "EQ701",   // Has dependants
+   "EQ702",   // Does not have dependants
+   "EQ703",   // Has dependants (children only)
+   "EQ704",   // Does not have dependants (children only)
+   "EQ705",   // Unknown
     "17",   // With dependants (overall)
     "18"   // Without dependants
   ],
   "Disability": [
+   "EQ401",   // Disabled
+   "EQ402",   // Not disabled
     "19",   // Yes
     "20"   // No
   ],
   "Ethnic group": [
+   "EQ1001",   // White
+   "EQ1002",   // Other
+   "EQ1003",   // Unknown
+   "EQ1004",   // Irish traveller
+   "EQ1005",   // Roma
+   "EQ1006",   // Indian
+   "EQ1007",   // Chinese
+   "EQ1008",   // Filipino
+   "EQ1009",   // Pakistani
+   "EQ1010",   // Arab
+   "EQ1011",   // Other Asian
+   "EQ1012",   // Black African
+   "EQ1013",   // Black other
+   "EQ1014",   // Mixed
+   "EQ1015",   // Non-white
     "21",   // White
     "22"   // Other
   ],
   "Sexual orientation": [
+   "EQ601",   // Straight or heterosexual
+   "EQ602",   // Gay, lesbian, bisexual or other 
+   "EQ603",   // Prefer not to say/not stated
+   "EQ604",   // Other
     "23",   // Heterosexual
     "24"   // Gay/lesbian, bisexual and other
   ],
@@ -80,11 +151,23 @@ var eqgroups = {
     "32"   // Mixed
   ],
   "Political opinion": [
+   "EQ901",   // Unionist
+   "EQ902",   // Nationalist
+   "EQ903",   // Neither
+   "EQ904",   // Other answer
     "33",   // Unionist
     "34",   // Nationalist
     "35"   // Neither
   ],
   "Religion": [
+    "EQ501",   // Catholic
+    "EQ507",   // Protestant
+    "EQ502",   // Protestant and other Christian 
+    "EQ503",   // Other
+    "EQ504",   // No religion
+    "EQ505",   // Other/No religion
+    "EQ506",   // Not stated
+    "EQ508",   // Other/None/Not known
     "14",   // Catholic
     "15",   // Protestant
     "16"   // Other/no religion
